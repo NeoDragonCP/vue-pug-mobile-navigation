395209ee9e7e306ee910b733f1d059cfa862baeb
NeoDragon <mcvickerstephen@gmail.com>
2020-10-04 23:53:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+#app
+  NavigationMobile
+  div(class="content", :class="{'open':showNav}")
+    .top-bar
+      i.fas.fa-bars#navigation-icon(v-if="mobileView && !showNav",@click="showNav = !showNav")
+      i.fas.fa-times#navigation-icon(v-if="mobileView && showNav",@click="showNav = !showNav")
+      Navigation(v-if="!mobileView")
+    Content
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+import NavigationMobile from "./components/NavigationMobile";
+import Content from "./components/Content";
+
+export default {
+  name: "App",
+  data: function() {
+    return {
+      mobileView: false,
+      showNav: false,
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 768;
+    },
+  },
+  created() {
+    this.handleView();
+    window.addEventListener("resize", this.handleView);
+  },
+  components: {
+    Navigation,
+    NavigationMobile,
+    Content,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  font-size: 1rem;
+}
+body {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma;
+  background-color: #7ca971;
+}
+#app {
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+  padding: 10px;
+  color: #333;
+
+  overflow: hidden;
+}
+.top-bar {
+  display: flex;
+  width: 100%;
+}
+#navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+  font-size: 2rem;
+}
+.content {
+  position: absolute;
+  top: 10px;
+  width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  transition: 0.3s transform cubic-bezier(0, 0.12, 0.14, 1);
+}
+.open {
+  transform: translateX(300px);
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  #content
+    .teaser
+      .teaser-content
+        | Read Article
+        i.fas.fa-chevron-right
+    .infobox
+      .title Articles
+      .value 128
+    .infobox
+      .title Readers
+      .value 18k
+    .infobox
+      .title Likes
+      .value 302k
+    .infobox
+      .title Comments
+      .value 145k
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.teaser {
+  position: relative;
+  width: 100%;
+  max-width: 768px;
+  height: 400px;
+  margin-bottom: 40px;
+  background-image: url("../assets/takeabreak.jpg");
+  background-size: cover;
+  background-position: top;
+  border-radius: 20px;
+  overflow: hidden;
+  .teaser-content {
+    position: absolute;
+    left: 20px;
+    bottom: 20px;
+    width: 210px;
+    height: 50px;
+    padding: 0 0 0 20px;
+    background-color: #111;
+    color: #efefef;
+    border-radius: 20px;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+  i {
+    margin-left: 0.5rem;
+    font-size: 1.6rem;
+  }
+}
+.infobox {
+  float: left;
+  width: 80px;
+  height: 80px;
+  padding: 10px;
+  margin: 0 20px 20px 0;
+  border-radius: 20px;
+  border: 1px solid #ccc;
+  .title {
+    font-weight: bold;
+  }
+  .value {
+    padding: 10px 0 0 0;
+    font-size: 2rem;
+  }
+}
+</style>
